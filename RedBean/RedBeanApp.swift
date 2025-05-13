42ccdc2496a679cf213e303f13adfb4e073066c7
angelLi17 <liangelfremont@gmail.com>
2025-05-12 21:43:55 -0700

    Initial Commit

---
+++ RedBean/RedBeanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedBeanApp.swift
+//  RedBean
+//
+//  Created by Angel on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RedBeanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RedBeanTests/RedBeanTests.swift
@@ -0,0 +1,16 @@
+//
+//  RedBeanTests.swift
+//  RedBeanTests
+//
+//  Created by Angel on 5/12/25.
+//
+
+import Testing
+
+struct RedBeanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
