190b29a3195fef4dec39ac040231ff101ec092a6
angelLi17 <liangelfremont@gmail.com>
2025-05-20 00:13:54 -0700

    auth is running!! ui now integrated with it as well (buttons and textfield)

--- RedBean/View/LoginView.swift
+++ RedBean/View/LoginView.swift
@@ -12,7 +12,7 @@ import FirebaseAuth
 struct LoginView: View {
     @StateObject private var viewModel = LoginViewModel()
     @Binding var currentScreen: AppScreen
-    @State var username: String = ""
+    @State var email: String = ""
     @State var password: String = ""
     
     var body: some View {
@@ -22,69 +22,63 @@ struct LoginView: View {
                 .foregroundColor(Color("aRed"))
                 .offset(x: 0, y: -159.50)
                 .multilineTextAlignment(.center)
-//            Text("Username")
-//                .font(.custom("Biryani", size: 20))
-//                .foregroundColor(Color("aRed"))
-//                .offset(x: -112, y: -81)
+            Text("Email")
+                .font(.custom("Biryani", size: 20))
+                .foregroundColor(Color("aRed"))
+                .offset(x: -134, y: -81)
             Rectangle()
                 .fill(Color("aPink"))
                 .frame(width: 322, height: 66)
                 .offset(x: 0, y: -38)
-//            Text("Password")
-//                .font(.custom("Biryani", size: 20))
-//                .foregroundColor(Color("aRed"))
-//                .offset(x: -113, y: 20.50)
+            Text("Password")
+                .font(.custom("Biryani", size: 20))
+                .foregroundColor(Color("aRed"))
+                .offset(x: -113, y: 20.50)
             Rectangle()
                 .fill(Color("aPink"))
                 .frame(width: 322, height: 66)
                 .offset(x: 0, y: 63)
-            Form {
-                TextField(text: $username, prompt: Text("redbeansoup@gmail.com")) {
-                    Text("Username")
-                        .font(.custom("Biryani", size: 20))
-                        .foregroundColor(Color("aRed"))
-                        .offset(x: -112, y: -81)
-                }
-                    .disableAutocorrection(true)
-                SecureField(text: $password, prompt: Text("qqmochi123")) {
-                    Text("Password")
-                        .font(.custom("Biryani", size: 20))
-                        .foregroundColor(Color("aRed"))
-                        .offset(x: -113, y: 20.50)
-                }
-                    .disableAutocorrection(true)
+            VStack {
+                TextField("",text: $email, prompt: Text("redbeansoup@gmail.com"))
+                    .font(Font.custom("Biryani", size: 20))
+                    .foregroundColor(Color("aRed"))
+                    .offset(x: 0, y: -2)
+                    .frame(width: 322, height: 66)
+                SecureField("",text: $password, prompt: Text("********"))
+                    .font(.custom("Biryani", size: 20))
+                    .foregroundColor(Color("aRed"))
+                    .offset(x: 0, y: 25)
+                    .frame(width: 322, height: 66)
             }
+            .disableAutocorrection(true)
 
-            RoundedRectangle(cornerRadius: 90)
-                .fill(Color("aRed"))
-                .frame(width: 322, height: 66)
-                .shadow(
-                    color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
-                )
-                .offset(x:0,y:166)
-            Text("LOGIN")
-                .font(.custom("Biryani", size: 30))
-                .foregroundColor(Color(red: 1, green: 0.87, blue: 0.83))
-                .offset(x: 0, y: 166)
-            Button(action:               {viewModel.signIn(username: username, password: password)}) {
+            Button(action: {viewModel.signIn(email: email, password: password)}) {
                 Text("Login")
-                    .font(.custom("Biryani", size: 30))
-                    .foregroundColor(Color(red: 1, green: 0.87, blue: 0.83))
-                    .offset(x: 0, y: 166)
+                    .font(.custom("Biryani", size: 25))
+                    .foregroundColor(Color("aPink"))
             }
-            .onAppear {
-                Auth.auth().addStateDidChangeListener { auth, user in
-                    if user != nil {
-                        //switch to home
-                    }
-                }
+            .background(Color("aRed"))
+            .cornerRadius(90)
+            .frame(width: 154, height: 66)
+            .shadow(
+                color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
+            )
+            .offset(x: -84, y: 166)
+            
+            Button(action: {viewModel.register(email: email, password: password)}) {
+                Text("Sign Up")
+                    .font(.custom("Biryani", size: 25))
+                    .foregroundColor(Color("aRed"))
             }
-            Button(action:               {viewModel.register(username: username, password: password)}) {
-                Text("Register")
-                    .font(.custom("Biryani", size: 30))
-                    .foregroundColor(Color(red: 1, green: 0.87, blue: 0.83))
-                    .offset(x: 0, y: 166)
-            }
+            .background(Color("aPink"))
+            .cornerRadius(90)
+            .frame(width: 159, height: 66)
+            .shadow(
+                color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
+            )
+            .offset(x: 82, y: 166)
+            
+            // note to self - start working tomorrow on the alert for login errors in LoginViewModel and adjusting the Button formatting. then try to add like database for contact list? do reset pw and confirm email later
         }
         .frame(width: 390, height: 844)
         .background(.white)
--- RedBean/ViewModel/LoginViewModel.swift
+++ RedBean/ViewModel/LoginViewModel.swift
@@ -12,9 +12,10 @@ import FirebaseCore
 import FirebaseAuth
 
 class LoginViewModel: ObservableObject {
+    @State var showLoginAlert = false
 
-    func signIn(username: String, password: String) {
-        let result: () = Auth.auth().signIn(withEmail: username, password: password) { result, error in
+    func signIn(email: String, password: String) {
+        let result: () = Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if error != nil {
                 print (error!.localizedDescription)
             }
@@ -22,10 +23,15 @@ class LoginViewModel: ObservableObject {
         
     }
     
-    func register(username: String, password: String) {
-        Auth.auth().createUser(withEmail: username, password: password) { result, error in
-            if error != nil {
-                print(error!.localizedDescription)
+    func register(email: String, password: String) {
+        var showalert = false
+        Auth.auth().createUser(withEmail: email, password: password) { [self] result, error in
+            if (error != nil) {
+                showLoginAlert = true
+            }
+            AlertScene("Sign Up Error", isPresented: $showLoginAlert) {
+            } message: {
+                Text(error!.localizedDescription)
             }
         }
         
