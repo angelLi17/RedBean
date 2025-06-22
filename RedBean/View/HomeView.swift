a761363d2881a46df65c7a4f14da8e1193ae9998
angelLi17 <liangelfremont@gmail.com>
2025-06-21 21:40:46 -0700

    fix firebase issue, work on home page to be clickable

--- RedBean/View/HomeView.swift
+++ RedBean/View/HomeView.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Angel on 5/18/25.
 //
-
 import SwiftUI
-
 struct HomeView: View {
     @State var menu1Clicked: Bool = false
     @State var menu2Clicked: Bool = false
@@ -32,66 +30,78 @@ struct HomeView: View {
                         .inset(by: 0.50)
                         .stroke(.black, lineWidth: 0.50)
                 )
-                .offset(x: 159.50, y: -385.50)
+                .onTapGesture {
+                    
+                }
+                .offset(x: 159.50, y: -370)
             Text(homeModel.displayName.isEmpty ? "Welcome!" : homeModel.displayName)
                 .font(.custom("Jua", size: 30))
+                .frame(width: 200, height: 40, alignment: .trailing)
+                .offset(x: 35, y: -370)
                 .foregroundColor(Color(.white))
-                .offset(x: -30, y: -383.50)
             Text("Love,\nRedBean.")
                 .font(Font.custom("Inspiration", size: 80))
                 .foregroundColor(.white)
-                .offset(x: -50, y: -48)
-            VStack(alignment: .leading, spacing: 25) {
+                .offset(x: -50, y: 0)
+            Color("aPink")
+                .cornerRadius(20)
+                .shadow(
+                    color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
+                )
+                .overlay(
+                    Text("serving our special menu:")
+                        .font(.custom("Biryani", size: 20))
+                        .foregroundColor(Color("aRed"))
+                        .padding(.bottom, 240)
+                )
+                .frame(width: 322, height: 278)
+                .offset(x: 0, y: 250)
+            VStack(spacing: 25) {
                 Text("✰start a new bean")
                     .font(Font.custom("Biryani", size: 30))
-                    .onHover { isHovered in
-                        if isHovered {
-                            menu1Clicked = true
-                        }
-                    }
-                    .foregroundColor(menu1Clicked ? Color(.gray).opacity(0.3) : Color(.white))
-                    .underline()
+                    .foregroundColor(Color(red: 0.84, green: 0.37, blue: 0.37))
+                    .background(menu1Clicked ? Color(.gray).opacity(0.3) : Color.clear)
+                    .underline(menu1Clicked ? true : false)
                     .onTapGesture {
+                        menu1Clicked.toggle()
                         //redirect to contact view
                     }
+                    .frame(width: 270, alignment: .leading)
                 Text("✰bean shredder")
                     .font(Font.custom("Biryani", size: 30))
                     .foregroundColor(Color(red: 0.84, green: 0.37, blue: 0.37))
+                    .background(menu2Clicked ? Color(.gray).opacity(0.3) : Color.clear)
+                    .underline(menu2Clicked ? true : false)
+                    .onTapGesture {
+                        menu2Clicked.toggle()
+                        //redirect to shred view
+                    }
+                    .frame(width: 270, alignment: .leading)
                 Text("✰photobooth")
                     .font(Font.custom("Biryani", size: 30))
                     .foregroundColor(Color(red: 0.84, green: 0.37, blue: 0.37))
+                    .background(menu3Clicked ? Color(.gray).opacity(0.3) : Color.clear)
+                    .underline(menu3Clicked ? true : false)
+                    .onTapGesture {
+                        menu3Clicked.toggle()
+                        //redirect to photobooth view
+                    }
+                    .frame(width: 270, alignment: .leading)
             }
-            .frame(width: 322, height: 278)
-            .offset(x: 0, y: 250)
+            .frame(width: 322, height: 230)
+            .offset(x: 0, y: 260)
             .background(
                 ZStack {
-                    Color("aPink")
-                        .cornerRadius(20)
-                        .shadow(
-                            color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
-                        )
-                        .overlay(
-                            Text("serving today's menu")
-                                .font(.custom("Biryani", size: 20))
-                                .foregroundColor(Color("aRed"))
-                                .padding(.bottom, 240)
-                        )
                     Color(.white)
                         .cornerRadius(10)
-                        .padding(.top, 32)
                         .padding(.horizontal, 16)
-                        .padding(.bottom, 16)
-                
-                        
                 }
-                .offset(x: 0, y: 250)
+                .offset(x: 0, y: 260)
             )
             
         }
     }
 }
-
 #Preview {
     HomeView(homeModel: HomeViewModel())
 }
-
