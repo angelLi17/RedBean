a0da3af55ce80e9f2c0d60281977a562bc78768c
angelLi17 <liangelfremont@gmail.com>
2025-07-09 15:29:51 -0700

    update

--- RedBean/View/ContactView.swift
+++ RedBean/View/ContactView.swift
@@ -12,58 +12,54 @@ struct ContactView: View {
     @ObservedObject var contactModel : ContactViewModel
     
     var body: some View {
-        ZStack() {
+        ZStack {
             GeometryReader { geometry in
                 Image("Bg")
                     .resizable()
                     .scaledToFill()
+                    .ignoresSafeArea()
                     .frame(width: geometry.size.width, height: geometry.size.height)
-            }
-            //header
-            Color("aPink")
-                .frame(width: 427, height: 50)
-                .shadow(
-                    color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
-                )
-                .opacity(0.9)
-                .offset(y: -370)
-            Text("Start a Bean")
-                .font(.custom("Jua", size: 30))
-                .foregroundColor(Color("aRed"))
-                .offset(x:-70,y:-370)
-            GeometryReader { geometry in
-                Image("HomeIcon")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 60, height: 60)
                     
             }
-            .offset(x:70,y:-370)
-//            .background{
-//                Color("aPink")
-//                    .frame(width: 60, height: 40)
-//                    .cornerRadius(20)
-//                    .offset(x:30,y: -370)
-//            }
-            
-            //footer
-            HStack(spacing:15) {
-                Rectangle()
-                    .frame(width:65, height:42)
-                    .foregroundColor(Color("aPink"))
-                    .cornerRadius(10)
-                Text("START")
-                    .foregroundColor(Color.white)
-                    .font(.custom("Biryani", size: 20))
-                    .frame(width:171, height:42)
-                    .background(Color("aRed"))
-                Rectangle()
-                    .frame(width:65, height:42)
-                    .foregroundColor(Color("aPink"))
-                    .cornerRadius(10)
+            VStack() {
+                //header
+                ZStack {
+                    GeometryReader { geometry in
+                        Color("aPink")
+                            .frame(width: 427, height: 50)
+                            .shadow(
+                                color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 2, y: 4
+                            )
+                            .opacity(0.9)
+                    }
+                    HStack(spacing: 70) {
+                        Text("Start a Bean")
+                            .font(.custom("Jua", size: 30))
+                            .foregroundColor(Color("aRed"))
+                        Image("HomeIcon")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 40)
+                    }
+                }
+                //footer
+                HStack(spacing:15) {
+                    Rectangle()
+                        .frame(width:65, height:42)
+                        .foregroundColor(Color("aPink"))
+                        .cornerRadius(10)
+                    Text("START")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Biryani", size: 20))
+                        .frame(width:171, height:42)
+                        .background(Color("aRed"))
+                    Rectangle()
+                        .frame(width:65, height:42)
+                        .foregroundColor(Color("aPink"))
+                        .cornerRadius(10)
+                }
+                
             }
-            .offset(y:365)
-            
 //            Button("Request Contacts Access") {
 //                contactModel.requestAccess()
 //            }
